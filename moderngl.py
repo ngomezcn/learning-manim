5873a588670e61364ca08b56e7e682af8cca9130
Naïm Gómez <castelldeneim@gmail.com>
2022-09-03 00:10:51 +0200

    d

---
+++ moderngl.py
@@ -0,0 +1,10 @@
+# test.py
+import moderngl_window as mglw
+
+class Test(mglw.WindowConfig):
+    gl_version = (3, 3)
+
+    def render(self, time, frametime):
+        self.ctx.clear(1.0, 0.0, 0.0, 0.0)
+
+Test.run()
